916093f58522ac01abc510e0c5f78851bc69a2ea
Ben Milnes <benjamin.milnes@nagwa.com>
2019-06-07 17:04:37 +0100

    Slight adjustments

--- 3DGraphing/script.py
+++ 3DGraphing/script.py
@@ -5,6 +5,7 @@ import math
 
 G = 6.67 * 10 ** -11  # Universal Gravitational Constant
 rho = 5510  # Density of Earth
+image = 4
 
 
 def sigmoid(x, a, b):
@@ -29,9 +30,11 @@ def accelerationDueToGravityOfAPlanet(x, y, cx, cy, radius, density):
     k = (4/3) * G * math.pi * density
 
     if x == cx and y == cy:
-        return 0  # Because 1 / x² is asymptotic at x = 0, return 0 here for when the radial position is 0 to avoid infinities
+        return 0  # Because 1 / x^{2} is asymptotic at x = 0, return 0 here for when the radial position is 0 to avoid infinities
 
     return k * (r * n + m * radius ** 3 / r ** 2)
+
+
 
 
 figure = plot.figure()
@@ -47,7 +50,15 @@ y1 = np.arange(0,  p, p/200)
 
 x2, y2 = np.meshgrid(x1, y1)
 
-z1 = np.array([accelerationDueToGravityOfAPlanet(x, y, 20, 20, radius, density) for x, y in zip(np.ravel(x2), np.ravel(y2))])
+if image == 1:
+        z1 = np.array([accelerationDueToGravityOfAPlanet(x, y, 20, 20, radius, density) for x, y in zip(np.ravel(x2), np.ravel(y2))])
+if image == 2:
+        z1 = np.array([accelerationDueToGravityOfAPlanet(x, y, 10, 30, radius / 2, density) + accelerationDueToGravityOfAPlanet(x, y, 25, 15, radius, density) for x, y in zip(np.ravel(x2), np.ravel(y2))])
+if image == 3:
+        z1 = np.array([accelerationDueToGravityOfAPlanet(x, y, 10, 30, radius / 1.5 , density / 2) + accelerationDueToGravityOfAPlanet(x, y, 30, 10, radius / 1.5, density) for x, y in zip(np.ravel(x2), np.ravel(y2))])
+if image == 4:
+        z1 = np.array([accelerationDueToGravityOfAPlanet(x, y, 10, 30, radius  , density ) + accelerationDueToGravityOfAPlanet(x, y, 30, 10, radius , density * 3) for x, y in zip(np.ravel(x2), np.ravel(y2))])
+
 z2 = z1.reshape(x2.shape)
 
 axes.set_zlim3d(20, -20)
